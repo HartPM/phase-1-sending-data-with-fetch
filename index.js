f78e6e657060858b6a327889962e60910d2691a5
Patrick Hart <pathart612@gmail.com>
2022-04-18 21:03:27 -0400

    commit tree be9873df2da6e9b4496b97474cdc9c61104ceb11

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => document.body.textContent = data.id)
+    .catch(error => document.body.textContent = error.message)
+};
+
+submitData('user24', 'Kale')
